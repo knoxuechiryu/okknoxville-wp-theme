998ec3a7e22de4e2f1a2716122f1caeeb201a15c
David Moore <sorakiu@gmail.com>
2017-04-22 16:24:16 -0400

    add fontawesome link

--- okknoxville/functions.php
+++ okknoxville/functions.php
@@ -1,5 +1,11 @@
 <?php
  
-// Your php code goes here
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+function enqueue_load_fa() {
+ 
+    wp_enqueue_style( 'load-fa', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+ 
+}
+
  
 ?>
